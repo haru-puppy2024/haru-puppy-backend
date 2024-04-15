4481d86e04ce1474d4fd546a5e334c753082ed3f
sejun Ahn <asjjun@naver.com>
2024-04-15 22:48:28 +0900

    Initial commit

---
+++ src/main/java/com/project/harupuppy/HaruPuppyApplication.java
@@ -0,0 +1,13 @@
+package com.project.harupuppy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruPuppyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruPuppyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/harupuppy/HaruPuppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.harupuppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruPuppyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
