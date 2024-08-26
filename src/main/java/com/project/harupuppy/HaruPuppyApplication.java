bcc709a9bb1e6be7cf4eb4a36ddec70813ffe827
sejun Ahn <asjjun@naver.com>
2024-08-27 02:43:09 +0900

    feat: 서버 TimeZone 한국 시간대로 설정

--- src/main/java/com/project/harupuppy/HaruPuppyApplication.java
+++ src/main/java/com/project/harupuppy/HaruPuppyApplication.java
@@ -1,8 +1,11 @@
 package com.project.harupuppy;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,8 @@ public class HaruPuppyApplication {
         SpringApplication.run(HaruPuppyApplication.class, args);
     }
 
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
